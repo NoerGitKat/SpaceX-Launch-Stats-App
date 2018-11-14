cc12804205bfa635fbac5477e26f17919175ef64
Noer Paanakker <noerpaanakker@Noers-MacBook-Pro-2.local>
2018-11-14 22:21:18 +0100

    added queries to RootQuery

--- schema.js
+++ schema.js
@@ -57,6 +57,35 @@ const RootQuery = new GraphQLObjectType({
           .get("https://api.spacexdata.com/v3/launches")
           .then(res => res.data);
       }
+    },
+    launch: {
+      type: LaunchType,
+      args: {
+        flight_number: { type: GraphQLInt }
+      },
+      resolve(parent, args) {
+        return axios
+          .get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+          .then(res => res.data);
+      }
+    },
+    rockets: {
+      type: RocketType,
+      args: {
+        id: { type: GraphQLInt }
+      },
+      resolve(parent, args) {
+        return axios.get("https://api.spacexdata.com/v3/rockets/");
+      }
+    },
+    rocket: {
+      type: RocketType,
+      args: {
+        id: { type: GraphQLInt }
+      },
+      resolve(parent, args) {
+        return axios.get(`https://api.spacexdata.com/v3/launches/${args.id}`);
+      }
     }
   }
 });
